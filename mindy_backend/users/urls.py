d5a0cb8173d9a6069ee0235fe0a4e91cd90b871f
Clearance <haoyanluo78@163.com>
2022-04-08 14:55:43 +0800

    delete z-pic/

--- mindy_backend/users/urls.py
+++ mindy_backend/users/urls.py
@@ -4,8 +4,8 @@ from .views import *
 
 app_name = 'users'
 urlpatterns = [
-    path('login/', login),
-    path('register', register),
+    path('', login),
+    path('register/', register),
     path('logout/', logout),
     path('confirm/', user_confirm),
 ]
